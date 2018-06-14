19225c458a7fc4781a6f49124c03ad6c1d4a65f5
Roger Bolser <carydeveloper@gmail.com>
2018-06-14 09:52:59 -0400

    Add form for messages and show incoming messages in browser

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -7,8 +7,9 @@ describe('generateMessage', () => {
     let from = 'Admin';
     let text = 'Test Text';
     let res = generateMessage(from, text);
-    expect(res.from).toBe(from)
-    expect(res.text).toBe(text)
+    // expect(res.from).toBe(from)
+    // expect(res.text).toBe(text)
+    expect(res).toMatchObject({from, text});
     expect(typeof res.createdAt).toBe('number');
   });
 });
